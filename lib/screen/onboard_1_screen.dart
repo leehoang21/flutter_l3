9d07dd6419a1bb7d705de374b011719ac4075da1
Huy Nguyễn <huycnttk16a@gmail.com>
2023-07-05 17:46:39 +0700

    add

--- lib/screen/onboard_1_screen.dart
+++ lib/screen/onboard_1_screen.dart
@@ -16,7 +16,7 @@ class _OnboardOneScreenState extends State<OnboardOneScreen>
   late final AnimationController _controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
   late final Animation<double> _animation = CurvedAnimation(
     parent: _controller,
-    curve: Curves.,
+    curve: Curves.linear,
   );
 
   @override
@@ -27,48 +27,57 @@ class _OnboardOneScreenState extends State<OnboardOneScreen>
 
   @override
   Widget build(BuildContext context) {
-    return FadeTransition(
-        opacity: _animation,
-        child: Scaffold(
-            body: Center(
-                child: Scrollbar(
-          child: SingleChildScrollView(
-            child: Column(children: [
-              Image.asset(Images.onboard_1),
-              Container(
-                  margin: const EdgeInsets.only(top: 40),
-                  child: const Text("Find you Comfort\nFood here",
-                      style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          height: 1.3),
-                      textAlign: TextAlign.center)),
-          Container(
-            margin: const EdgeInsets.fromLTRB(64, 20, 64, 0),
-            child: const Text(
-              "Here You Can find a chef or dish for every taste and color. Enjoy!",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 12, height: 1.3),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 40),
-            child: ElevatedButton(
-              onPressed: () {
-                Get.offNamed(RouteHelper.onBoarding2);
-              },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 18, 40, 18),
-                    child: Text("Next", style: TextStyle(fontSize: 16)),
-                  ),
-                  style: TextButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
+    return AnimatedBuilder(
+        animation: _animation,
+        builder: (BuildContext context, Widget? child) {
+          return Scaffold(
+              body: Center(
+                  child: Scrollbar(
+            child: SingleChildScrollView(
+              child: Column(children: [
+                Container(
+                  height: 400,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(Images.background_sign_in),
+                          fit: BoxFit.fill)),
+                ),
+                // Image.asset(Images.onboard_1)
+                Container(
+                    margin: const EdgeInsets.only(top: 40),
+                    child: const Text("Find you Comfort\nFood here",
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            height: 1.3),
+                        textAlign: TextAlign.center)),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(64, 20, 64, 0),
+                  child: const Text(
+                    "Here You Can find a chef or dish for every taste and color. Enjoy!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12, height: 1.3),
                   ),
                 ),
-              )
-            ]),
-          ),
-        ))));
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Get.offNamed(RouteHelper.onBoarding2);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(40, 18, 40, 18),
+                      child: Text("Next", style: TextStyle(fontSize: 16)),
+                    ),
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                    ),
+                  ),
+                )
+              ]),
+            ),
+          )));
+        });
   }
 }
--- lib/screen/onboard_2_screen.dart
+++ lib/screen/onboard_2_screen.dart
@@ -11,47 +11,65 @@ class OnboardTwoScreen extends StatefulWidget {
   State<OnboardTwoScreen> createState() => _OnboardTwoScreenState();
 }
 
-class _OnboardTwoScreenState extends State<OnboardTwoScreen> {
+class _OnboardTwoScreenState extends State<OnboardTwoScreen> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.linear,
+  );
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Scrollbar(
-      child: SingleChildScrollView(
-        child: Column(children: [
-          Image.asset(Images.onboard_2),
-          Container(
-              margin: const EdgeInsets.only(top: 40),
-              child: const Text("Food Ninja is Where Your\n Comfort Food Lives",
-                  style: TextStyle(
-                      fontSize: 22, fontWeight: FontWeight.bold, height: 1.3),
-                  textAlign: TextAlign.center)),
-          Container(
-            margin: const EdgeInsets.fromLTRB(64, 20, 64, 0),
-            child: const Text(
-              "Enjoy a fast and smooth food delivery at your doorstep",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 12, height: 1.3),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 40),
-            child: ElevatedButton(
-              onPressed: () {
-                Get.offNamed(RouteHelper.signIn);
-              },
-              child: Container(
-                padding: EdgeInsets.fromLTRB(40, 18, 40, 18),
-                child: Text("Next", style: TextStyle(fontSize: 16)),
-              ),
-              style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
-              ),
-            ),
-          )
-        ]),
-      ),
-    )));
+    return AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return   Scaffold(
+              body: Center(
+                  child: Scrollbar(
+                    child: SingleChildScrollView(
+                      child: Column(children: [
+                        Container(
+                          height: 400,
+                          decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage(Images.background_sign_in),
+                                  fit: BoxFit.fill)),
+                        ),
+                        // Image.asset(Images.onboard_2),
+                        Container(
+                            margin: const EdgeInsets.only(top: 40),
+                            child: const Text("Food Ninja is Where Your\n Comfort Food Lives",
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.bold, height: 1.3),
+                                textAlign: TextAlign.center)),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(64, 20, 64, 0),
+                          child: const Text(
+                            "Enjoy a fast and smooth food delivery at your doorstep",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 12, height: 1.3),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 40),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Get.offNamed(RouteHelper.signIn);
+                            },
+                            child: Container(
+                              padding: EdgeInsets.fromLTRB(40, 18, 40, 18),
+                              child: Text("Next", style: TextStyle(fontSize: 16)),
+                            ),
+                            style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                            ),
+                          ),
+                        )
+                      ]),
+                    ),
+                  )));
+        },);
+
+
   }
 }
