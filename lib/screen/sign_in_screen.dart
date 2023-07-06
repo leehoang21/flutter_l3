2da40fcfec74ddb367672a539410b32e9ca30d49
gtuan201 <106383317+gtuan201@users.noreply.github.com>
2023-07-06 17:52:06 +0700

    base

--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -25,109 +25,120 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
         body: Stack(
           children: [
-            GetBuilder<AuthController>(
-                builder: (controller) => Opacity(
-                  opacity: controller.loading ? 0.3 : 1,
-                  child: Container(
-                    color: Colors.white,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(100, 56, 100, 60),
-                          color: Colors.white,
-                          child: Image.asset(Images.logo),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                          child: Column(
-                            children: [
-                              const Text(
-                                "Login To Your Account",
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 20),
-                                child: TextFormField(
-                                  controller: _usernameController,
-                                  decoration: InputDecoration(
-                                      contentPadding:
-                                      const EdgeInsets.only(left: 28),
-                                      border: OutlineInputBorder(
-                                          borderSide: const BorderSide(
-                                              width: 1,
-                                              color:
-                                              Color.fromRGBO(244, 244, 244, 1)),
-                                          borderRadius: BorderRadius.circular(15)),
-                                      hintText: "Email",
-                                      hintStyle: const TextStyle(
-                                          color: Colors.grey)),
+            Scrollbar(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(height: 100,),
+                      GetBuilder<AuthController>(
+                        builder: (controller) => Opacity(
+                          opacity: controller.loading ? 0.3 : 1,
+                          child: Container(
+                            color: Colors.white,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.fromLTRB(100, 56, 100, 60),
+                                  color: Colors.white,
+                                  child: Image.asset(Images.logo),
                                 ),
-                              ),
-                              Container(
-                                  margin: const EdgeInsets.only(top: 20),
-                                  child: Obx(
-                                        () => TextFormField(
-                                      obscureText: _showPass.value,
-                                      controller: _passwordController,
-                                      decoration: InputDecoration(
-                                          contentPadding:
-                                          const EdgeInsets.only(left: 28),
-                                          suffixIcon: IconButton(
-                                            onPressed: () {
-                                              _showPass.value = !_showPass.value;
-                                            },
-                                            icon: Icon(_showPass.value
-                                                ? Icons.visibility
-                                                : Icons.visibility_off),
-                                          ),
-                                          border: OutlineInputBorder(
-                                            borderSide: const BorderSide(
-                                                width: 1,
-                                                color: Color.fromRGBO(
-                                                    244, 244, 244, 1)),
-                                            borderRadius: BorderRadius.circular(15),
-                                          ),
-                                          hintText: "Password",
-                                          hintStyle: const TextStyle(
-                                              color:
-                                              Colors.grey)),
-                                    ),
-                                  )),
-                              Container(
-                                margin: const EdgeInsets.only(top: 40),
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    _login();
-                                  },
-                                  style: TextButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(15)),
-                                  ),
-                                  child: Container(
-                                    padding:
-                                    const EdgeInsets.fromLTRB(40, 18, 40, 18),
-                                    child: const Text("Login",
+                                Container(
+                                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                                  child: Column(
+                                    children: [
+                                      const Text(
+                                        "Login To Your Account",
                                         style: TextStyle(
-                                            fontSize: 16,
-                                            color: Color.fromRGBO(
-                                                191, 252, 226, 1.0))),
+                                            fontSize: 20,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 20),
+                                        child: TextFormField(
+                                          controller: _usernameController,
+                                          textInputAction: TextInputAction.next,
+                                          decoration: InputDecoration(
+                                              contentPadding:
+                                              const EdgeInsets.only(left: 28),
+                                              border: OutlineInputBorder(
+                                                  borderSide: const BorderSide(
+                                                      width: 1,
+                                                      color:
+                                                      Color.fromRGBO(244, 244, 244, 1)),
+                                                  borderRadius: BorderRadius.circular(15)),
+                                              hintText: "Email",
+                                              hintStyle: const TextStyle(
+                                                  color: Colors.grey)),
+                                        ),
+                                      ),
+                                      Container(
+                                          margin: const EdgeInsets.only(top: 20),
+                                          child: Obx(
+                                                () => TextFormField(
+                                                  textInputAction: TextInputAction.done,
+                                                  obscureText: _showPass.value,
+                                                  controller: _passwordController,
+                                                  decoration: InputDecoration(
+                                                  contentPadding:
+                                                  const EdgeInsets.only(left: 28),
+                                                  suffixIcon: IconButton(
+                                                    onPressed: () {
+                                                      _showPass.value = !_showPass.value;
+                                                    },
+                                                    icon: Icon(_showPass.value
+                                                        ? Icons.visibility
+                                                        : Icons.visibility_off),
+                                                  ),
+                                                  border: OutlineInputBorder(
+                                                    borderSide: const BorderSide(
+                                                        width: 1,
+                                                        color: Color.fromRGBO(
+                                                            244, 244, 244, 1)),
+                                                    borderRadius: BorderRadius.circular(15),
+                                                  ),
+                                                  hintText: "Password",
+                                                  hintStyle: const TextStyle(
+                                                      color:
+                                                      Colors.grey)),
+                                            ),
+                                          )),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 40),
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            _login();
+                                          },
+                                          style: TextButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(15)),
+                                          ),
+                                          child: Container(
+                                            padding:
+                                            const EdgeInsets.fromLTRB(40, 18, 40, 18),
+                                            child: const Text("Login",
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Color.fromRGBO(
+                                                        191, 252, 226, 1.0))),
+                                          ),
+                                        ),
+                                      )
+                                    ],
                                   ),
                                 ),
-                              )
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ),
+                )
             ),
             Center(
               child: GetBuilder<AuthController>(
